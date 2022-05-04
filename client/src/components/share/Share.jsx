fdd3fd535293fedb6b444ececb0d43fa791d88de
juanhuaan <juanhuaa@usc.edu>
2022-05-03 22:02:28 -0700

    add tags function

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -13,14 +13,19 @@ import axios from "axios";
 export default function Share() {
   const { user } = useContext(AuthContext);
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const tags = useRef();
   const desc = useRef();
   const [file, setFile] = useState(null);
-
+  
   const submitHandler = async (e) => {
     e.preventDefault();
+    let tagsArr = [];
+    tagsArr = tags.current.value.split(",");
+    let newArr = tagsArr.map(val => val.toLowerCase().replace(/\s/g, ''))
     const newPost = {
       userId: user._id,
       desc: desc.current.value,
+      tags: newArr
     };
     if (file) {
       const data = new FormData();
@@ -41,6 +46,8 @@ export default function Share() {
     } catch (err) { }
   };
 
+  
+
   return (
     <div className="share">
       <div className="shareWrapper">
@@ -54,11 +61,23 @@ export default function Share() {
             }
             alt=""
           />
-          <input
-            placeholder={"What's in your mind " + user.username + "?"}
+         <form className="shareForm">
+         <input
+            placeholder={"Hi " +  user.username + ", what's happening?"}
             className="shareInput"
             ref={desc}
-          />
+          /> 
+          <div className="shareOption">
+              <Label htmlColor="#CD5C5C" className="shareIcon" />
+              <input
+                placeholder={"# tags: weather, mood, study, travel, cat, ..."}
+                type="text"
+                className="tagInput"
+                ref={tags}
+                autocomplete="text"
+            />
+            </div>  
+         </form>
         </div>
         <hr className="shareHr" />
         {file && (
@@ -67,6 +86,7 @@ export default function Share() {
             <Cancel className="shareCancelImg" onClick={() => setFile(null)} />
           </div>
         )}
+
         <form className="shareBottom" onSubmit={submitHandler}>
           <div className="shareOptions">
             <label htmlFor="file" className="shareOption">
@@ -80,10 +100,10 @@ export default function Share() {
                 onChange={(e) => setFile(e.target.files[0])}
               />
             </label>
-            <div className="shareOption">
+            {/* <div className="shareOption">
               <Label htmlColor="blue" className="shareIcon" />
-              <span className="shareOptionText">Tag</span>
-            </div>
+              <span className="shareOptionText">Tag</span>  
+            </div> */}
             <div className="shareOption">
               <Room htmlColor="green" className="shareIcon" />
               <span className="shareOptionText">Location</span>
