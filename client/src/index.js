4399ace3ebbb70c00561b362d665705f4aaaed23
Zeyu Liu <390940545@qq.com>
2022-05-11 12:07:22 -0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://env-v1.eba-pce2husc.us-west-2.elasticbeanstalk.com/api"
 
 ReactDOM.render(
   <React.StrictMode>
